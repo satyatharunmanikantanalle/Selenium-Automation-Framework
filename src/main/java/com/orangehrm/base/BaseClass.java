352c9680d9c5d44bdc860812caac0d81766dd45c
Satya Nalle <satyanalle143@gmail.com>
2026-06-27 12:36:15 +0530

    New Changes

--- src/main/java/com/orangehrm/base/BaseClass.java
+++ src/main/java/com/orangehrm/base/BaseClass.java
@@ -32,7 +32,7 @@ import com.orangehrm.utilities.LoggerManager;
 public class BaseClass {
 
 	protected static Properties prop;
-	// protected static WebDriver driver;  code commented
+	// protected static WebDriver driver;
 	// private static ActionDriver actionDriver;
 
 	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
